82a94dc4f9aa90751d670fb59f0e368eaed4f358
yliaalei <ylia.alei@mail.ru>
2025-09-13 18:17:49 +0300

    Create firebase-config.js

---
+++ docs/firebase-config.js
@@ -0,0 +1,9 @@
+// Замените на свои данные из Firebase Console
+const firebaseConfig = {
+  apiKey: "ВАШ_API_KEY",
+  authDomain: "ВАШ_ДОМЕН.firebaseapp.com",
+  projectId: "ВАШ_PROJECT_ID",
+  storageBucket: "ВАШ_BUCKET.appspot.com",
+  messagingSenderId: "ВАШ_SENDER_ID",
+  appId: "ВАШ_APP_ID"
+};
