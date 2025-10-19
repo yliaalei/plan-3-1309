410ec6d0ed700f2f2f1e9affa1b28be7355079df
yliaalei <ylia.alei@mail.ru>
2025-10-19 21:55:03 +0300

    Update firebase-config.js

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -1,9 +1,8 @@
-// Замените на свои данные из Firebase Console
 const firebaseConfig = {
   apiKey: "AIzaSyAqUEri9DzMftxtS7ker4tfC-EnZNK6nMA",
   authDomain: "content-planner-ffb8e.firebaseapp.com",
   projectId: "content-planner-ffb8e",
-  storageBucket: "content-planner-ffb8e.firebasestorage.app",
+  storageBucket: "content-planner-ffb8e.appspot.com",
   messagingSenderId: "615520592527",
   appId: "1:615520592527:web:7478b8c3de904c924086fa"
 };
