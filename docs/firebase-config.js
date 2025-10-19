c7e5fd8c7e074570cc96943b4c41eb2768e9785a
yliaalei <ylia.alei@mail.ru>
2025-10-20 00:56:15 +0300

    Update firebase-config.js

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -10,5 +10,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
 const db = firebase.firestore();
+const auth = firebase.auth();
